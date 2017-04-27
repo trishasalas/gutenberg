59f366ae7134f401d9dfd8f27340ff2455dbdf1a
BE-Webdesign <cromleyedwin89@gmail.com>
2017-04-27 12:32:13 -0400

    Fixes #515. Add keyboard navigation to the inserter. This introduces RxJS as a library, which could be very useful for other aspects of this project. Accessibility review would be awesome.

--- editor/components/inserter/index.js
+++ editor/components/inserter/index.js
@@ -7,6 +7,7 @@ import IconButton from 'components/icon-button';
 class Inserter extends wp.element.Component {
 	constructor() {
 		super( ...arguments );
+		this.nodes = {};
 		this.toggle = this.toggle.bind( this );
 		this.close = this.close.bind( this );
 		this.state = {
@@ -15,6 +16,10 @@ class Inserter extends wp.element.Component {
 	}
 
 	toggle() {
+		if ( this.state.opened === true ) {
+			this.nodes.toggle.focus();
+		}
+
 		this.setState( {
 			opened: ! this.state.opened
 		} );
@@ -36,8 +41,12 @@ class Inserter extends wp.element.Component {
 					icon="insert"
 					label={ wp.i18n.__( 'Insert block' ) }
 					onClick={ this.toggle }
-					className="editor-inserter__toggle" />
-				{ opened && <InserterMenu position={ position } onSelect={ this.close } /> }
+					className="editor-inserter__toggle"
+					aria-haspopup="true"
+					id="inserter-toggle"
+					buttonRef={ ( node ) => this.nodes.toggle = node }
+					/>
+				{ opened && <InserterMenu position={ position } onSelect={ this.close } closeMenu={ this.toggle } /> }
 			</div>
 		);
 	}
