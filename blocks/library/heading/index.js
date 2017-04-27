d8d69923872ae9542b41b2b576f6b29a4722901d
Riad Benguella <benguella@gmail.com>
2017-04-27 10:21:01 +0100

    Quote block: Update transforms since the content is now a React.children object
    - Avoid data loss while switching blocks

--- blocks/library/heading/index.js
+++ blocks/library/heading/index.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { isString } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import { registerBlock, query } from 'api';
@@ -38,9 +43,9 @@ registerBlock( 'core/heading', {
 				blocks: [ 'core/text' ],
 				transform: ( { content, align } ) => {
 					if ( Array.isArray( content ) ) {
-						// TODO this appears to always be true?
-						// TODO reject the switch if more than one paragraph
-						content = content[ 0 ];
+						content = wp.element.concatChildren( content.map( ( elt ) =>
+							! elt || isString( elt ) || elt.type !== 'p' ? elt : elt.props.children
+						) );
 					}
 					return {
 						nodeName: 'H2',
@@ -56,7 +61,7 @@ registerBlock( 'core/heading', {
 				blocks: [ 'core/text' ],
 				transform: ( { content, align } ) => {
 					return {
-						content: [ content ],
+						content,
 						align
 					};
 				}
